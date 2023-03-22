6f26901f45c9b79e9531a480ca080c37eac6dd59
Elad Menahem <93273498+EladMe1@users.noreply.github.com>
2023-03-22 21:02:30 +0200

    Update Vector.cpp

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,26 +1,38 @@
 #include "..\include\vector.h"
 
+/**
+	* @brief Constructs a new Vector with a given size and value for all elements.
+	* @param size The size of the vector.
+	* @param value The value of all elements in the vector.
+	*/
 Vector::Vector(size_t size, int value) : m_size(size), m_capacity(size), m_value(new int[size])
 {
 	for (int i = 0; i < size; i++)
 		m_value[i] = value;
 }
 
-//destractor
+/**
+	 * @brief Destroys the Vector and frees the memory.
+	 */
 Vector::~Vector()
 {
 	delete[] m_value;
 }
 
-//copy constractor
+/**
+	 * @brief Constructs a new Vector by copying an existing Vector.
+	 * @param other The Vector to be copied.
+	 */
 Vector::Vector(const Vector& other) : m_size(other.m_size), m_capacity(other.m_capacity), m_value(new int[m_capacity])
 {
 	for (int i = 0; i < m_capacity && i < m_size; i++) {
 		m_value[i] = other.m_value[i];
 	}
 }
-
-//moving constractor
+/**
+	* @brief Constructs a new Vector by moving an existing Vector.
+	* @param other The Vector to be moved.
+	*/
 Vector::Vector(Vector&& other) noexcept : m_size(other.m_size), m_capacity(other.m_capacity), m_value(other.m_value)
 {
 	other.m_size = 0;
@@ -28,6 +40,11 @@ Vector::Vector(Vector&& other) noexcept : m_size(other.m_size), m_capacity(other
 	other.m_value = nullptr;
 }
 
+/**
+	* @brief Copies the elements of one Vector into another Vector.
+	* @param other The Vector to be copied.
+	* @return A reference to the current Vector.
+	*/
 Vector& Vector::operator=(const Vector& other)
 {
 	if (this != &other)
@@ -38,6 +55,12 @@ Vector& Vector::operator=(const Vector& other)
 	return *this;
 
 }
+
+/**
+   * @brief Compares two vectors lexicographically.
+   * @param other The Vector to be compared to.
+   * @return A std::strong_ordering value that indicates the relationship between the two vectors.
+   */
 
 std::strong_ordering Vector::operator<=>(const Vector& other) const noexcept
 {
@@ -54,7 +77,12 @@ std::strong_ordering Vector::operator<=>(const Vector& other) const noexcept
 	}
 }
 
-
+/**
+	 * @brief Gets a reference to an element of the Vector by index.
+	 * @param index The index of the element to retrieve.
+	 * @return A reference to the element at the specified index.
+	 * @throws std::out_of_range if the index is out of bounds.
+	 */
 
 int& Vector::operator[](const int index)
 {
@@ -66,6 +94,13 @@ int& Vector::operator[](const int index)
 	return m_value[index];
 }
 
+/**
+   * @brief Gets a const reference to an element of the Vector by index.
+   * @param index The index of the element to retrieve.
+   * @return A const reference to the element at the specified index.
+   * @throws std::out_of_range if the index is out of bounds.
+   */
+
 const int& Vector::operator[](const int index) const
 {
 	if (index >= m_size)
@@ -75,6 +110,12 @@ const int& Vector::operator[](const int index) const
 
 	return m_value[index];
 }
+
+/**
+	 * @brief Checks if two Vectors are equal.
+	 * @param other The Vector to be compared to.
+	 * @return True if the Vectors are equal, false otherwise.
+	 */
 
 const bool& Vector::operator==(const Vector& other) const noexcept
 {
@@ -89,11 +130,21 @@ const bool& Vector::operator==(const Vector& other) const noexcept
 
 }
 
+/**
+	* @brief Checks if two Vectors are not equal.
+	* @param other The Vector to be compared to.
+	* @return True if the Vectors are not equal, false otherwise.
+	*/
+
 const bool& Vector::operator!=(const Vector& other) const noexcept
 {
 	return !(this->operator==(other));
 }
 
+/**
+	* @brief Swaps the contents of two Vectors.
+	* @param other The Vector to swap with.
+	*/
 
 void Vector::swap(Vector& other) noexcept
 {
@@ -102,26 +153,45 @@ void Vector::swap(Vector& other) noexcept
 	std::swap(m_capacity, other.m_capacity);
 }
 
+/**
+* Returns a pointer to the underlying data of the vector.
+* @return Pointer to the underlying data.
+*/
 int* Vector::data() const
 {
 	return m_value;
 }
 
+/**
+* Checks if the vector is empty.
+* @return True if the vector is empty, false otherwise.
+*/
 bool Vector::empty() const
 {
 	return (m_size == 0);
 }
 
+/**
+* Returns the number of elements in the vector.
+* @return Number of elements in the vector.
+*/
 int Vector::size() const
 {
 	return m_size;
 }
 
+/**
+* Returns the capacity of the vector.
+* @return Capacity of the vector.
+*/
 int Vector::capacity() const
 {
 	return m_capacity;
 }
 
+/**
+* Clears the vector by setting its size, capacity, and underlying data to zero/nullptr.
+*/
 void Vector::clear()
 {
 	m_size = 0;
@@ -129,7 +199,10 @@ void Vector::clear()
 	m_value = nullptr;
 }
 
-
+/**
+* Reserves capacity for the vector.
+* @param capacity The new capacity to reserve for the vector.
+*/
 void Vector::reserve(size_t capacity)
 {
 	if (capacity <= m_capacity) {
@@ -148,7 +221,11 @@ void Vector::reserve(size_t capacity)
 }
 
 
-
+/**
+* Resizes the vector.
+* @param size The new size of the vector.
+* @param value The value to fill the new elements with.
+*/
 void Vector::resize(int size, int value)
 {
 	if (size > m_size) {
@@ -162,6 +239,10 @@ void Vector::resize(int size, int value)
 	m_size = size;
 }
 
+/**
+* Appends a new element to the end of the vector.
+* @param value The value of the new element to add to the vector.
+*/
 void Vector::push_back(int&& value)
 {
 	if (m_size == 0)
@@ -177,6 +258,9 @@ void Vector::push_back(int&& value)
 
 }
 
+/**
+* Removes the last element from the vector.
+*/
 void Vector::pop_back()
 {
 	if (m_size == 0)
@@ -186,6 +270,9 @@ void Vector::pop_back()
 
 }
 
+/**
+* insert element to the vector.
+*/
 void Vector::insert(size_t index, int value)
 {
 	try {
@@ -211,7 +298,9 @@ void Vector::insert(size_t index, int value)
 	}
 }
 
-
+/**
+* delete the last element from the vector.
+*/
 void Vector::erase(int index)
 {
 	try {
